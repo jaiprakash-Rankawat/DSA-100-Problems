d69c4f9d0192dbbddf3c8584777d159f80f94bd1
Jai Prakash Rankawat <jaiparkashrankawat2@gmail.com>
2024-08-26 15:43:26 +0530

    problem 8 on dsa

---
+++ Problem 8/app.js
@@ -0,0 +1,9 @@
+// Q8. write a function to reverse a number ?
+
+function reverse(number) {
+  let strNum = number.toString().split("").reverse().join("");
+  return parseInt(strNum);
+}
+let number = reverse(12345);
+console.log(number); //54321
+console.log(typeof number); // type is number
