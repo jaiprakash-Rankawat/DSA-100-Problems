05b3db4f8ac3aa383c8ac962e1a032ed05495c33
Jai Prakash Rankawat <jaiparkashrankawat2@gmail.com>
2024-08-29 19:05:52 +0530

    LinkedList problems

--- Problem 013/app.js
+++ Problem 013/app.js
@@ -15,14 +15,109 @@ class LinkedList {
   // pushing items to linkedlist
   push(item) {
     let nextElement = new Node(item);
+
+    if (!this.head) {
+      this.head = nextElement;
+      this.tail = nextElement;
+    }
     this.tail.next = nextElement;
     this.tail = nextElement;
     this.length++;
   }
+
+  // deleting last element
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let temp = this.head;
+    let prev = this.head;
+
+    while (temp.next) {
+      prev = temp;
+      temp = prev.next;
+    }
+
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp;
+  }
+
+  //  adding element at first positon
+  unshift(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  // removing element from first positon
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    this.head = this.head.next;
+    this.length--;
+    if (this.length == 0) {
+      this.tail = null;
+      this.head = null;
+    }
+  }
+
+  // getting first element
+  first() {
+    if (this.length == 0) {
+      this.head = null;
+    }
+    return this.head;
+  }
+
+  // getting last element
+  last() {
+    if (this.length == 0) {
+      this.tail = null;
+    }
+    return this.tail;
+  }
+
+  // get element by index
+  element(index) {
+    let counter = 0;
+    let temp = this.head;
+    while (temp) {
+      if (counter == index) {
+        return temp;
+      }
+      temp = temp.next;
+      counter++;
+    }
+  }
+
+  // update value
+  update(index, value) {
+    let temp = this.element(index);
+    if (temp) {
+      temp.data = value;
+      return true;
+    }
+    return false;
+  }
 }
 
-let link = new LinkedList(5);
-link.push(10);
-link.push(20);
-link.push(30);
-console.log(link);
+let linkedList = new LinkedList(5);
+console.log(linkedList);
